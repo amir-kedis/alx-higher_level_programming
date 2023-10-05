9bcd9ba212e22be967cc066bcddcda059a8c44a2
amir-kedis <amir.kedis@gmail.com>
2023-10-05 22:26:15 +0300

    0x03 Python - import modules - task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
 
     if argc == 1:
         print("0 arguments.")
-    elif argc > 1:
+    elif argc == 2:
+        print("1 argument:")
+    elif argc > 2:
         print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
         print("{}: {}".format(i, argv[i]))
