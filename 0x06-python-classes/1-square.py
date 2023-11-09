343b6f7b0d9013b742d670f206a9577dd0b22ef5
amir-kedis <amir.kedis@gmail.com>
2023-11-09 23:40:24 +0200

    0x06 Python - classes - task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square class with size."""
+
+
+class Square:
+    """Square with size."""
+
+    def __init__(self, size=0):
+        """Initialize the data."""
+        self.__size = size
