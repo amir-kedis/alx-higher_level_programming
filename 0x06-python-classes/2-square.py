ea4d8ab35361909805ffc597d8212618e805444c
amir-kedis <amir.kedis@gmail.com>
2023-11-09 23:59:54 +0200

    0x06 Python - classes - task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class with size."""
+
+
+class Square:
+    """Square with size."""
+
+    def __init__(self, size=0):
+        """Initialize the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
