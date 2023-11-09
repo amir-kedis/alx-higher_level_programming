a50315da00fc9ecc2394aa2ca98bf564a2064f33
amir-kedis <amir.kedis@gmail.com>
2023-11-10 00:07:49 +0200

    0x06 Python - classes - task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Square class with size."""
+
+
+class Square:
+    """Square with size."""
+
+    def __init__(self, size=0):
+        """Initialize the data."""
+        self.__size = size
+
+    @property  # getter
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter  # setter
+    def size(self, value):
+        """Set the size to value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size**2
+
+    def my_print(self):
+        """Print the square with #."""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
