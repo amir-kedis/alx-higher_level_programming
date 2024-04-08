3c6deed067c64f6d66e8b71b5009905e608f88cf
amir-kedis <amir.kedis@gmail.com>
2024-04-08 14:46:59 +0200

    JS - warmup - task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log("Not a number");
+} else {
+  const num = parseInt(args[2]);
+  if (isNaN(num)) {
+    console.log("Not a number");
+  } else {
+    console.log(`My number: ${num}`);
+  }
+}
