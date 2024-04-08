4e64eb26886c3a9d616a57d3dfb808aba645ef3f
amir-kedis <amir.kedis@gmail.com>
2024-04-08 14:50:50 +0200

    JS - warmup - task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log("Missing size");
+} else {
+  const size = parseInt(args[2]);
+  if (isNaN(size)) {
+    console.log("Missing size");
+  } else {
+    for (let i = 0; i < size; i++) {
+      console.log("X".repeat(size));
+    }
+  }
+}
