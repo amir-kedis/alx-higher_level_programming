9e34e68870b4cb8e3f92dd081a7c0dc9cf928d30
amir-kedis <amir.kedis@gmail.com>
2024-04-08 14:52:10 +0200

    JS - warmup - task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+function add(a, b) {
+  return a + b;
+}
+
+if (args[2] === undefined || args[3] === undefined) {
+  console.log("NaN");
+} else {
+  console.log(add(parseInt(args[2]), parseInt(args[3])));
+}
