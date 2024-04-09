fb1ceb6ddef1da9ae9ac46f6f20d091973049772
amir-kedis <amir.kedis@gmail.com>
2024-04-09 12:10:30 +0200

    0x013 - JS objects, scopes, closures - tasks 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-function nbOccurences (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.filter((element) => element === searchElement).length;
-}
-
-module.exports = nbOccurences;
+};
